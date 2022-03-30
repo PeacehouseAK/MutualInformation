cac14a9a7ad4382f14d4d418500fc4e884922fd9
PeacehouseAK <89989541+PeacehouseAK@users.noreply.github.com>
2022-03-30 15:20:17 +0530

    Add files via upload

---
+++ stack1.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+int func(char *str)
+{
+  int variable_a;
+  char buffer[12];
+  /* Fill in code here to print the address of
+   * the array "buffer".
+   * Your code here:
+   */
+
+  strcpy(buffer, str);
+  
+  return 1;
+}
+
+int main(int argc, char **argv)
+{
+  char *buf = "hello\n";
+  
+  if(argc > 1){
+    buf = argv[1];
+  }
+
+  func(buf);
+  printf("Returned Properly\n");
+  return 1;
+}
